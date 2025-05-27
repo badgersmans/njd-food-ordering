9258060caf1f32d9fe807256f7a208020e5cfb05
Shawn Law <shawnlaw6669@gmail.com>
2025-05-27 08:51:45 +0800

    show how many things in cart

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,15 +1,25 @@
 import { Link, Stack } from 'expo-router'
-import {FontAwesome5, Ionicons} from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { useCart } from '@/providers/CartProvider';
+import Colors from '@/constants/Colors';
 
 export default function MenuLayout() {
+  const {items} = useCart()
+
   return (
     <Stack
       screenOptions={{
         headerRight: () => (
           <Link href={'/cart'} asChild>
-            <TouchableOpacity>
-              <Ionicons name="cart-outline" size={20} color={'black'} />
+            <TouchableOpacity style={styles.cartContainer}>
+              <Ionicons name="cart-outline" size={24} color={'black'} />
+              {items.length > 0 && (
+                <View style={styles.cartCounter}>
+                  <Text style={styles.countText}>{items.length}</Text>
+                </View>
+              )}
+              
             </TouchableOpacity>
           </Link>
         )
@@ -24,3 +34,25 @@ export default function MenuLayout() {
     </Stack>
   )
 }
+
+const styles = StyleSheet.create({
+  cartContainer: {
+    // backgroundColor: 'red',
+    width: 30
+  },
+  cartCounter: {
+    position: 'absolute',
+    backgroundColor: Colors.light.tint,
+    bottom: -4,
+    right: 0,
+    // width: 16,
+    alignItems: 'center',
+    borderRadius: 100,
+    padding: 1
+  },
+  countText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '600'
+  }
+})
